33896a824463c89bf363d76e26d24696b5ebc9a8
Luis Felipe Alves <felipe_alvess@hotmail.com>
2018-11-27 22:45:37 -0200

    estrutura inicial do projeto, com angular 7 e teste de pequenos componentes

---
+++ ng-movies/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule} from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+
+import { AngularSvgIconModule } from 'angular-svg-icon';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AngularSvgIconModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
